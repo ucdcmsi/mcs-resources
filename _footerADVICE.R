d8ab4607c7d057d2682db69f54076a88f44d365f
Priya Shukla <prishukla.sci@gmail.com>
2020-07-17 18:48:41 -0700

    oops forgot to push this file :)

--- _footerADVICE.R
+++ _footerADVICE.R
@@ -1,5 +1,5 @@
 # --- AFTER EDITING FOOTER --- #
 # --- BEFORE rmarkdown::render_site()' --- #
 
-#RUN IN CONSOLE: 
+#RUN IN CONSOLE TO RENDER HTML FILE: 
   # rmarkdown::render("_footer.Rmd", output_format = "html_fragment")
